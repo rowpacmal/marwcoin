d2d1cbc8d187242246687d8abd2ec05d322dbb7d
rowpacmal <rowpacmal@outlook.com>
2024-05-17 15:47:49 +0200

    Added a constructor to the Block class

--- backend/classes/Block.mjs
+++ backend/classes/Block.mjs
@@ -1,5 +1,13 @@
 const Block = class {
-	constructor() {}
+	constructor({ index, timestamp, prevHash, hash, data, nonce, difficulty }) {
+		this.index = index;
+		this.timestamp = timestamp;
+		this.prevHash = prevHash;
+		this.hash = hash;
+		this.data = data;
+		this.nonce = nonce;
+		this.difficulty = difficulty;
+	}
 };
 
 export default Block;
