790c99c37282c711307fae5216343cbd560b0f78
lisce18 <willelovgrenolofsson@gmail.com>
2024-05-18 16:02:52 +0200

    Created a function for errorhandling to a log

--- backend/app.mjs
+++ backend/app.mjs
@@ -1,6 +1,7 @@
-import express from "express";
-import helmet from "helmet";
-import cors from "cors";
+import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+import errorHandler from './middlewares/errorHandler.mjs';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,9 +10,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(cors());
+app.use(errorHandler);
 
 app.listen(PORT, (err) => {
-    err
-        ? console.error("Failed to run server..")
-        : console.log(`Server running at port ${PORT}..`);
+  err
+    ? console.error('Failed to run server..')
+    : console.log(`Server running at port ${PORT}..`);
 });
