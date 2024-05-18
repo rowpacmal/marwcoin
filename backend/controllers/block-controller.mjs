ffef4cadeac08cbbc98a6b7418ba75532fa87d18
rowpacmal <rowpacmal@outlook.com>
2024-05-18 11:38:03 +0200

    Added getBlockByIndex() func; Improve error handle for test

--- backend/controllers/block-controller.mjs
+++ backend/controllers/block-controller.mjs
@@ -3,15 +3,44 @@ import Block from '../classes/Block.mjs';
 // Dummy data for testing...
 const blockchain = { chain: [Block.genesis] };
 
+export const getBlockByIndex = (req, res, next) => {
+	const block = blockchain.chain.find((b) => b.index === req.params.index);
+
+	if (!block) {
+		// for testing purposes
+		res.status(404).json({
+			error:
+				'An error occurred when searching for the block. There is no block with that index...',
+		});
+		return;
+
+		// return next(
+		// 	new Error(
+		// 		'An error occurred when searching for the block. There is no block with that index...'
+		// 	)
+		// );
+	}
+
+	res.status(200).json({ data: block });
+};
+
 export const createBlock = (req, res, next) => {
 	const data = req.body;
 
-	if (!(data instanceof Array))
-		return next(
-			new Error(
-				'An error occurred when handling the data. The data is an invalid payload...'
-			)
-		);
+	if (!(data instanceof Array)) {
+		// for testing purposes
+		res.status(400).json({
+			error:
+				'An error occurred when handling the data. The data is an invalid payload...',
+		});
+		return;
+
+		// return next(
+		// 	new Error(
+		// 		'An error occurred when handling the data. The data is an invalid payload...'
+		// 	)
+		// );
+	}
 
 	const prevBlock = blockchain.chain.at(-1);
 	const block = Block.mineBlock({ prevBlock, data });
