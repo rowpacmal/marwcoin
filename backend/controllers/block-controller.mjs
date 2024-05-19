27c56d810f989b7844cbbd08eb7f6a623ed3d433
rowpacmal <rowpacmal@outlook.com>
2024-05-19 20:32:07 +0200

    Removed test code and comments

--- backend/controllers/block-controller.mjs
+++ backend/controllers/block-controller.mjs
@@ -1,24 +1,15 @@
+import { blockchain } from '../app.mjs';
 import Block from '../classes/Block.mjs';
-
-// Dummy data for testing...
-const blockchain = { chain: [Block.genesis] };
 
 export const getBlockByIndex = (req, res, next) => {
 	const index = +req.params.index;
 	const block = blockchain.chain.find((b) => b.index === index);
 
 	if (!block) {
-		// for testing purposes
 		res.status(404).json({
 			error: `An error occurred when searching for the block. There is no block with the index ${index}...`,
 		});
 		return;
-
-		// return next(
-		// 	new Error(
-		// 		'An error occurred when searching for the block. There is no block with that index...'
-		// 	)
-		// );
 	}
 
 	res.status(200).json({ data: block });
@@ -34,18 +25,11 @@ export const createBlock = (req, res, next) => {
 	const data = req.body;
 
 	if (!(data instanceof Array)) {
-		// for testing purposes
 		res.status(400).json({
 			error:
 				'An error occurred when handling the data. The data is an invalid payload...',
 		});
 		return;
-
-		// return next(
-		// 	new Error(
-		// 		'An error occurred when handling the data. The data is an invalid payload...'
-		// 	)
-		// );
 	}
 
 	const prevBlock = blockchain.chain.at(-1);
