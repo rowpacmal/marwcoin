96941b9887cd0e98775942ce7fa57da3ad138a89
rowpacmal <rowpacmal@outlook.com>
2024-05-19 22:02:21 +0200

    Improve response handling with classes/models

--- backend/controllers/block-controller.mjs
+++ backend/controllers/block-controller.mjs
@@ -1,35 +1,50 @@
 import { blockchain } from '../app.mjs';
 import Block from '../classes/Block.mjs';
+import ErrorResponse from '../classes/ErrorResponse.mjs';
+import ResponseData from '../classes/ResponseData.mjs';
 
 export const getBlockByIndex = (req, res, next) => {
 	const index = +req.params.index;
 	const block = blockchain.chain.find((b) => b.index === index);
 
 	if (!block) {
-		res.status(404).json({
-			error: `An error occurred when searching for the block. There is no block with the index ${index}...`,
-		});
-		return;
+		return next(
+			new ErrorResponse(
+				`An error occurred when searching for the block. There is no block with the index ${index}...`,
+				404
+			)
+		);
 	}
 
-	res.status(200).json({ data: block });
+	res
+		.status(200)
+		.json(
+			new ResponseData(
+				`Successfully find a block with the index ${index}.`,
+				200,
+				block
+			)
+		);
 };
 
 export const getLatestBlock = (req, res, next) => {
 	const block = blockchain.chain.at(-1);
 
-	res.status(200).json({ data: block });
+	res
+		.status(200)
+		.json(new ResponseData('Successfully find the latest block.', 200, block));
 };
 
 export const createBlock = (req, res, next) => {
 	const data = req.body;
 
 	if (!(data instanceof Array)) {
-		res.status(400).json({
-			error:
+		return next(
+			new ErrorResponse(
 				'An error occurred when handling the data. The data is an invalid payload...',
-		});
-		return;
+				400
+			)
+		);
 	}
 
 	const prevBlock = blockchain.chain.at(-1);
@@ -37,5 +52,7 @@ export const createBlock = (req, res, next) => {
 
 	blockchain.chain.push(block);
 
-	res.status(201).json({ data: block });
+	res
+		.status(201)
+		.json(new ResponseData('Successfully mined a block.', 201, block));
 };
