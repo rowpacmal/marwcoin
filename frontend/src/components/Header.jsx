7478b9d7dff1145f50c5a90353eac873c1a4ac11
rowpacmal <rowpacmal@outlook.com>
2024-05-19 15:23:22 +0200

    Added header style

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,33 +1,44 @@
 import { NavLink } from 'react-router-dom';
+import './Header.css';
 
 export const Header = () => {
 	return (
-		<header>
-			<figure>
+		<header className="header">
+			<NavLink to="/" className="header-logo">
 				<img
 					src="./src/assets/Logo3.png"
 					alt="Marw Logo"
 					width="2408"
 					height="1208"
 				/>
-			</figure>
+			</NavLink>
 
-			<nav>
-				<ul>
-					<li>
-						<NavLink to="">Home</NavLink>
+			<nav className="header-nav">
+				<ul className="header-nav-ul">
+					<li className="header-nav-li">
+						<NavLink to="/" className="header-nav-li-link">
+							Home
+						</NavLink>
 					</li>
-					<li>
-						<NavLink to="">Transaction</NavLink>
+					<li className="header-nav-li">
+						<NavLink to="/transaction" className="header-nav-li-link">
+							Transaction
+						</NavLink>
 					</li>
-					<li>
-						<NavLink to="">Blocks</NavLink>
+					<li className="header-nav-li">
+						<NavLink to="/blocks" className="header-nav-li-link">
+							Blocks
+						</NavLink>
 					</li>
-					<li>
-						<NavLink to="">About</NavLink>
+					<li className="header-nav-li">
+						<NavLink to="/about" className="header-nav-li-link">
+							About
+						</NavLink>
 					</li>
-					<li>
-						<NavLink to="">Contact</NavLink>
+					<li className="header-nav-li">
+						<NavLink to="/contact" className="header-nav-li-link">
+							Contact
+						</NavLink>
 					</li>
 				</ul>
 			</nav>
