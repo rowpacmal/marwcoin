a20268f967a68fb782137c217db03e946d08a66d
rowpacmal <rowpacmal@outlook.com>
2024-05-19 16:39:58 +0200

    Update Header component and css

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,47 +1,53 @@
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './Header.css';
 
 export const Header = () => {
 	return (
 		<header className="header">
-			<NavLink to="/" className="header-logo">
-				<img
-					src="./src/assets/Logo3.png"
-					alt="Marw Logo"
-					width="2408"
-					height="1208"
-				/>
-			</NavLink>
+			<div className="header-wrapper">
+				<Link to="/" className="header-logo">
+					<img
+						src="./src/assets/Logo3.png"
+						alt="Marw Logo"
+						width="2408"
+						height="1208"
+					/>
+				</Link>
 
-			<nav className="header-nav">
-				<ul className="header-nav-ul">
-					<li className="header-nav-li">
-						<NavLink to="/" className="header-nav-li-link">
-							Home
-						</NavLink>
-					</li>
-					<li className="header-nav-li">
-						<NavLink to="/transaction" className="header-nav-li-link">
-							Transaction
-						</NavLink>
-					</li>
-					<li className="header-nav-li">
-						<NavLink to="/blocks" className="header-nav-li-link">
-							Blocks
-						</NavLink>
-					</li>
-					<li className="header-nav-li">
-						<NavLink to="/about" className="header-nav-li-link">
-							About
-						</NavLink>
-					</li>
-					<li className="header-nav-li">
-						<NavLink to="/contact" className="header-nav-li-link">
-							Contact
-						</NavLink>
-					</li>
-				</ul>
-			</nav>
+				<nav className="header-nav">
+					<ul className="header-nav-ul">
+						<li className="header-nav-li">
+							<NavLink to="/" className="header-nav-link">
+								Home
+							</NavLink>
+						</li>
+
+						<li className="header-nav-li">
+							<NavLink to="/transaction" className="header-nav-link">
+								Transaction
+							</NavLink>
+						</li>
+
+						<li className="header-nav-li">
+							<NavLink to="/blocks" className="header-nav-link">
+								Blocks
+							</NavLink>
+						</li>
+
+						<li className="header-nav-li">
+							<NavLink to="/about" className="header-nav-link">
+								About
+							</NavLink>
+						</li>
+
+						<li className="header-nav-li">
+							<NavLink to="/contact" className="header-nav-link">
+								Contact
+							</NavLink>
+						</li>
+					</ul>
+				</nav>
+			</div>
 		</header>
 	);
 };
