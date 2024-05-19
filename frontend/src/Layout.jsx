d452b2de2432910f0f89d2c1af83a2432e5d4cc8
rowpacmal <rowpacmal@outlook.com>
2024-05-19 16:40:25 +0200

    Update Layout and App.css

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -1,9 +1,11 @@
+import { Footer } from './components/Footer';
 import { Header } from './components/Header';
 
 export const Layout = () => {
 	return (
 		<>
 			<Header />
+			<Footer />
 		</>
 	);
 };
