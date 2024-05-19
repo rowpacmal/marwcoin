61655e90fe66f4915aa9079ec2e2e7391bd08744
rowpacmal <rowpacmal@outlook.com>
2024-05-19 21:19:08 +0200

    Fixed minor logic error

--- backend/classes/ErrorResponse.mjs
+++ backend/classes/ErrorResponse.mjs
@@ -1,7 +1,7 @@
 class ErrorResponse extends Error {
 	constructor(message, statusCode) {
 		super(message);
-		this.statusCode = this.statusCode;
+		this.statusCode = statusCode;
 		this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
 	}
 }
