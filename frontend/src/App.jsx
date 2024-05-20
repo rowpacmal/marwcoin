40bf651975d404bde810d17c76f7073860f80bec
Ahmed Abbasi <contact@ahmed.dev>
2024-05-20 21:10:17 +0200

    Configure `@tanstack/react-query` in `App.jsx`

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
 
 const App = () => {
+    const queryClient = new QueryClient();
     return (
         <>
-            <RouterProvider router={router} />
+            <QueryClientProvider client={queryClient}>
+                <RouterProvider router={router} />
+            </QueryClientProvider>
         </>
     );
 };
