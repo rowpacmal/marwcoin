b6cc36264eaced01f995acc811c5b970e7de876a
rowpacmal <rowpacmal@outlook.com>
2024-05-22 13:46:11 +0200

    Move files; update path

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import styles from './Footer.module.css';
+import styles from '../../assets/styles/Footer.module.css';
 
 export const Footer = () => {
 	return (
--- frontend/src/components/layout/Main.jsx
+++ frontend/src/components/layout/Main.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import styles from './Main.module.css';
+import styles from '../../assets/styles/Main.module.css';
 
 export const Main = () => {
 	return (
