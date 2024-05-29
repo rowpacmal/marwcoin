cca9d26fe5937413f76efba1dab5299cc4317a9b
rowpacmal <rowpacmal@outlook.com>
2024-05-29 17:37:28 +0200

    remove unused imports

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import styles from '../assets/styles/Home.module.css';
 
 export const Home = () => {
--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export const NotFound = () => {
-  return <h2>Oj, något gick fel. Sidan kunde inte hittas.</h2>;
+	return <h2>Oj, något gick fel. Sidan kunde inte hittas.</h2>;
 };
