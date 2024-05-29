a58b927017098b93c1c36bf414789b553e1a758f
Ahmed Abbasi <contact@ahmed.dev>
2024-05-29 20:26:27 +0200

    Add module for gracefully saving the in-memory blockchain to `./data` directory upon shutdown

---
+++ backend/shutdown.mjs
@@ -0,0 +1,11 @@
+import { saveBlockchain } from "./blockchainIO.mjs";
+import { blockchain } from "./startup.mjs";
+
+const handleProcessExit = (blockchain) => {
+    console.log("Process terminated. Saving blockchain...");
+    saveBlockchain(blockchain);
+    process.exit(0);
+}
+
+process.on('SIGINT', () => handleProcessExit(blockchain));
+process.on('SIGTERM', () => handleProcessExit(blockchain));
