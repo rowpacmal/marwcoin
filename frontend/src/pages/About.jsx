755f403e89895d3a080b7087535cf39ceb210e27
rowpacmal <rowpacmal@outlook.com>
2024-05-29 17:35:41 +0200

    add credits&license; improve img size; add about page img

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -2,28 +2,28 @@ import React from 'react';
 import styles from '../assets/styles/About.module.css';
 
 export const About = () => {
-  return (
-    <div className={styles.aboutContainer}>
-      <div className={styles.textContainer}>
-        <h1 className={styles.title}>About</h1>
-        <p className={styles.description}>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum,
-          excepturi?
-        </p>
-        <p className={styles.breadText}>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere,
-          laboriosam a. Ipsum, consectetur esse voluptates exercitationem fugit
-          laudantium omnis laboriosam?
-        </p>
-      </div>
-      <img
-        src='./src/assets/home-page.jpg'
-        alt='Graffiti on a wall'
-        width='2408'
-        height='1208'
-        className={styles.aboutImage}
-      />
-      <h1 className={styles.titleSmallScreen}>About</h1>
-    </div>
-  );
+	return (
+		<div className={styles.aboutContainer}>
+			<div className={styles.textContainer}>
+				<h1 className={styles.title}>About</h1>
+				<p className={styles.description}>
+					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum,
+					excepturi?
+				</p>
+				<p className={styles.breadText}>
+					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere,
+					laboriosam a. Ipsum, consectetur esse voluptates exercitationem fugit
+					laudantium omnis laboriosam?
+				</p>
+			</div>
+			<img
+				src="./src/assets/about-page.webp"
+				alt="Graffiti on a wall"
+				width="2408"
+				height="1208"
+				className={styles.aboutImage}
+			/>
+			<h1 className={styles.titleSmallScreen}>About</h1>
+		</div>
+	);
 };
